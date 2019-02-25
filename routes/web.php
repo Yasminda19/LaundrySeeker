dc5ccddc6e43f2adc4370e216b8d3fe47dbef4b2
zeroload <zeroload@mail.ru>
2019-02-25 10:44:31 +0700

    hang-on notice

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/aaa', function () {
+    return view('aaa');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
