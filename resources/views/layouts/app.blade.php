648239bd3bbc558ff3af89c6fc396cf46de9464e
zeroload <zeroload@mail.ru>
2019-04-22 11:43:57 +0700

    [WIP] tambahin menu buat launderer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,6 +49,11 @@
                                 </li>
                             @endif
                         @else
+                            <li class="nav-item">
+                                @if (Auth::user()->type === "launderer")
+                                    <a class="nav-link" href="#">Opsi khusus launderer</a>
+                                @endif
+                            </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
