4486d9529793b8a377ec78424a3d1f9b2a10842e
zeroload <zeroload@mail.ru>
2019-05-01 19:12:41 +0700

    apa yang dilakukan /login sekarang bisa membuatmu tercengang, fitur tambahan Nomor 2 bikin kaget

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -38,6 +38,12 @@
                             </span>
                         @endif
                     </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                        <label class="form-check-label" for="remember">
+                            {{ __('Ingat saya') }}
+                        </label>
+                    </div>
                     <br>
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
